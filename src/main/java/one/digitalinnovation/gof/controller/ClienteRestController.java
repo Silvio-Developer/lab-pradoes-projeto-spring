ffdbc6e340fc96bef21a79df09ed53a777dd50b9
Silvio-Developer <sgomes.dev@gmail.com>
2022-08-04 18:12:02 -0300

    Adicionando conteúdo do curso FullStack Santander DIO com base nas implementações do Tech Lead Venilton FalvoJr

---
+++ src/main/java/one/digitalinnovation/gof/controller/ClienteRestController.java
@@ -0,0 +1,69 @@
+package one.digitalinnovation.gof.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import one.digitalinnovation.gof.model.Cliente;
+import one.digitalinnovation.gof.service.ClienteService;
+
+import java.util.Iterator;
+import java.util.Objects;
+
+/**
+ * Esse {@link RestController} representa nossa <b>Facade</b>, pois abstrai toda
+ * a complexidade de integrações (Banco de Dados H2 e API do ViaCEP) em uma
+ * interface simples e coesa (API REST).
+ * 
+ * @author falvojr
+ */
+@RestController
+@RequestMapping("clientes")
+public class ClienteRestController {
+
+	@Autowired
+	private ClienteService clienteService;
+
+	@GetMapping
+	public ResponseEntity<Iterable<Cliente>> buscarTodos() {
+		return ResponseEntity.ok(clienteService.buscarTodos());
+	}
+	@GetMapping("/{name}")
+	public Cliente clienteNome(String nome) {
+		for (Cliente cliente : clienteService.buscarTodos()){
+			if (Objects.equals(cliente.getNome().toLowerCase(), nome.toLowerCase()))
+				return cliente;
+		}
+		return null;
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Cliente> buscarPorId(@PathVariable Long id) {
+		return ResponseEntity.ok(clienteService.buscarPorId(id));
+	}
+
+	@PostMapping
+	public ResponseEntity<Cliente> inserir(@RequestBody Cliente cliente) {
+		clienteService.inserir(cliente);
+		return ResponseEntity.ok(cliente);
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<Cliente> atualizar(@PathVariable Long id, @RequestBody Cliente cliente) {
+		clienteService.atualizar(id, cliente);
+		return ResponseEntity.ok(cliente);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deletar(@PathVariable Long id) {
+		clienteService.deletar(id);
+		return ResponseEntity.ok().build();
+	}
+}
---
+++ src/main/java/one/digitalinnovation/gof/service/ClienteService.java
@@ -0,0 +1,27 @@
+package one.digitalinnovation.gof.service;
+
+import one.digitalinnovation.gof.model.Cliente;
+import one.digitalinnovation.gof.model.Endereco;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+/**
+ * Interface que define o padrão <b>Strategy</b> no domínio de cliente. Com
+ * isso, se necessário, podemos ter multiplas implementações dessa mesma
+ * interface.
+ *
+ * @author Silvio
+ */
+public interface ClienteService {
+
+    Iterable<Cliente> buscarTodos();
+
+    Cliente buscarPorId(Long id);
+
+    void inserir(Cliente cliente);
+
+    void atualizar(Long id, Cliente cliente);
+
+    void deletar(Long id);
+
+}
